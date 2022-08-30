07b136033e0d5281611813e74ab6405e4b77c11f
mmheydariy <112417909+mmheydariy@users.noreply.github.com>
2022-08-30 02:49:47 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var totop = document.querySelector(".to-top");
+        window.addEventListener("scroll", () => {
+            if(window.pageYOffset > 100){
+                totop.classList.add("active");
+            }else {
+                totop.classList.remove("active");
+            }
+        })
